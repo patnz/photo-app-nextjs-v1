521e7f483a0a84be5557eff387bef048a4576514
Pat Wilson <pat@wilsons.org.nz>
2023-04-08 16:22:19 +1200

    basic nav, main

---
+++ src/pages/components/main.tsx
@@ -0,0 +1,17 @@
+export function Main() {
+  return (
+    <>
+      <main className="w-4xl mx-auto">
+        <section
+          id="hero"
+          className="flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12"
+        >
+          <article className="sm:w-.5" />
+          <h2 className="max-w-md text-4xl font-bold text-center sm:text-5sl sm:text-left text-slate-900 dark:text-white">
+            A collection of photography over the years...
+          </h2>
+        </section>
+      </main>
+    </>
+  )
+}
---
+++ src/pages/components/nav.tsx
@@ -0,0 +1,30 @@
+export function Nav() {
+  return (
+    <header className="bg-yellow-900 text-slate-100 sticky top-0 z-8">
+      <section className="max-w-4xl mx-auto p-4 flex justify-around items-center">
+        <h1 className="text-3xl font-medium">
+          <a href="#hero">📷</a> pat wilson photography
+        </h1>
+        <div>
+          <button
+            id="mobile-open-button"
+            className="text-3xl text-white sm:hidden focus:outline-none z-9"
+          >
+            &#9776;
+          </button>
+          <nav className="hidden sm:block space-x-8 text-xl" aria-label="main">
+            <a href="#gallery" className="hover:opacity-90">
+              Gallery
+            </a>
+            <a href="#testimonials" className="hover:opacity-90">
+              Testimonials
+            </a>
+            <a href="#contact" className="hover:opacity-90">
+              Contact
+            </a>
+          </nav>
+        </div>
+      </section>
+    </header>
+  )
+}
