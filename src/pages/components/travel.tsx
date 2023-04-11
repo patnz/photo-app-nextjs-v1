901a472e5dc08bb875794a114bdf5092f11c4cad
Pat Wilson <pat@wilsons.org.nz>
2023-04-11 23:05:58 +1200

    testing123

--- src/pages/components/travel.tsx
+++ src/pages/components/travel.tsx
@@ -1,6 +1,34 @@
 import Image from 'next/image'
+import { motion, useScroll } from 'framer-motion'
 
 export function Travel() {
+  // TESTING FRAMER-MOTION
+  //
+
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.5,
+        delay: 3,
+      },
+    },
+  }
+
+  const item = {
+    hidden: { opacity: 0, x: 2000 },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: 5,
+      },
+    },
+  }
+  //
+  //
+
   return (
     <>
       <section
@@ -11,16 +39,27 @@ export function Travel() {
           Travel
         </h2>
         <ul className="list-none mx-auto my-12 flex flex-col sm:flex-row justify-around gap-5">
-          <li className="md:w-1/5 sm:w-5/6 flex flex-col items-center border dark:border-stone-500 bg-white dark:bg-black py-2 px-2 rounded-3xl shadow-xl">
+          <motion.li
+            variants={item}
+            initial={item.hidden}
+            animate={item.show}
+            className="md:w-1/5 sm:w-5/6 flex flex-col items-center border dark:border-stone-500 bg-white dark:bg-black py-2 px-2 rounded-3xl shadow-xl"
+          >
             <Image
               src="/images/gallery-card-3.jpeg"
               alt="placeholder-alt-tag"
               width="2000"
               height="3000"
               className="w-100 my-0 mx-1 rounded-2xl"
+              loading="eager"
             />
-          </li>
-          <li className="md:w-1/5 sm:w-5/6 flex flex-col items-center border dark:border-stone-500 bg-white dark:bg-black py-2 px-2 rounded-3xl shadow-xl">
+          </motion.li>
+          <motion.li
+            variants={item}
+            initial="hidden"
+            animate="show"
+            className="md:w-1/5 sm:w-5/6 flex flex-col items-center border dark:border-stone-500 bg-white dark:bg-black py-2 px-2 rounded-3xl shadow-xl"
+          >
             <Image
               src="/images/gallery-card-4.jpeg"
               alt="placeholder-alt-tag"
@@ -28,8 +67,13 @@ export function Travel() {
               height="3000"
               className="w-100 my-0 mx-1 rounded-2xl"
             />
-          </li>
-          <li className="md:w-1/5 sm:w-5/6 flex flex-col items-center border dark:border-stone-500 bg-white dark:bg-black py-2 px-2 rounded-3xl shadow-xl">
+          </motion.li>
+          <motion.li
+            variants={item}
+            initial="hidden"
+            animate="show"
+            className="md:w-1/5 sm:w-5/6 flex flex-col items-center border dark:border-stone-500 bg-white dark:bg-black py-2 px-2 rounded-3xl shadow-xl"
+          >
             <Image
               src="/images/gallery-card-1.jpeg"
               alt="placeholder-alt-tag"
@@ -37,8 +81,13 @@ export function Travel() {
               height="3000"
               className="w-90 mb-0 mx-1 rounded-2xl"
             />
-          </li>
-          <li className="md:w-1/5 sm:w-5/6 flex flex-col items-center border dark:border-stone-500 bg-white dark:bg-black py-2 px-2 rounded-3xl shadow-xl">
+          </motion.li>
+          <motion.li
+            variants={item}
+            initial="hidden"
+            animate="show"
+            className="md:w-1/5 sm:w-5/6 flex flex-col items-center border dark:border-stone-500 bg-white dark:bg-black py-2 px-2 rounded-3xl shadow-xl"
+          >
             <Image
               src="/images/gallery-card-2.jpeg"
               alt="placeholder-alt-tag"
@@ -46,7 +95,7 @@ export function Travel() {
               height="3000"
               className="w-90 mb-0 mx-1 rounded-2xl"
             />
-          </li>
+          </motion.li>
         </ul>
       </section>
     </>
