eca4846629090f8ca340c20eef5923dc6b798f6e
Pat Wilson <pat@wilsons.org.nz>
2023-04-14 09:57:12 +1200

    fiddling

--- src/pages/components/festivals.tsx
+++ src/pages/components/festivals.tsx
@@ -7,7 +7,7 @@ export function Festival() {
         id="festivals"
         className="p-6 my-12 scroll-mt-20 widescreen:section-min-height tallscreen:section-min-height"
       >
-        <h2 className="text-4xl font-bold text-right pr-60 sm:text-5xl mb-6 text-stone-800 dark:text-stone-100">
+        <h2 className="text-4xl font-bold text-center sm:text-right sm:pr-60 sm:text-5xl mb-6 text-stone-800 dark:text-stone-100">
           Festivals
         </h2>
         <ul className="list-none mx-auto my-12 flex flex-col sm:flex-row justify-around gap-5">
