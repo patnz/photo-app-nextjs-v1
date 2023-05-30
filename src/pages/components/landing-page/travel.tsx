7814a0f87b21def1dbfb78011bfa20067c7f448a
Pat Wilson <pat@wilsons.org.nz>
2023-05-30 11:00:11 +1000

    new assets. style adjustment.

--- src/pages/components/landing-page/travel.tsx
+++ src/pages/components/landing-page/travel.tsx
@@ -21,41 +21,41 @@ export function Travel() {
         <ul className="list-none mx-auto my-12 flex flex-col sm:flex-row justify-around gap-5">
           <li className="md:w-1/5 sm:w-5/6 flex flex-col items-center border-2 border-transparent dark:border-rose-950 bg-white dark:bg-stone-900 py-2 px-2 rounded-3xl shadow-xl">
             <Image
-              src="/images/travel-card-3.jpeg"
+              src="/images/portrait-seville-7.jpeg"
               alt="placeholder-alt-tag"
-              width="2000"
-              height="3000"
-              className="w-100 my-0 mx-1 rounded-2xl hover:border-transparent hover:border"
+              width="1000"
+              height="1000"
+              className="h-full object-cover my-0 mx-1 rounded-2xl hover:border-transparent hover:border"
               loading="eager"
             />
           </li>
           <li className="md:w-1/5 sm:w-5/6 flex flex-col items-center border-2 border-transparent dark:border-rose-950 bg-white dark:bg-stone-900 py-2 px-2 rounded-3xl shadow-xl">
             <Image
-              src="/images/travel-card-4.jpeg"
+              src="/images/portrait-seville-5.jpeg"
               alt="placeholder-alt-tag"
-              width="2000"
-              height="3000"
-              className="w-100 my-0 mx-1 rounded-2xl hover:border-transparent hover:border"
+              width="1000"
+              height="1000"
+              className="h-full object-cover my-0 mx-1 rounded-2xl hover:border-transparent hover:border"
               loading="eager"
             />
           </li>
           <li className="md:w-1/5 sm:w-5/6 flex flex-col items-center border-2 border-transparent dark:border-rose-950 bg-white dark:bg-stone-900 py-2 px-2 rounded-3xl shadow-xl">
             <Image
-              src="/images/travel-card-8.jpeg"
+              src="/images/portrait-seville-3.jpeg"
               alt="placeholder-alt-tag"
-              width="2000"
-              height="3000"
-              className="w-100 mb-0 mx-1 rounded-2xl hover:border-transparent hover:border"
+              width="1000"
+              height="1000"
+              className="h-full object-cover my-0 mx-1 rounded-2xl hover:border-transparent hover:border"
               loading="eager"
             />
           </li>
           <li className="md:w-1/5 sm:w-5/6 flex flex-col items-center border-2 border dark:border-rose-950 bg-white dark:bg-stone-900 py-2 px-2 rounded-3xl shadow-xl">
             <Image
-              src="/images/travel-card-2.jpeg"
+              src="/images/portrait-seville-6.jpeg"
               alt="placeholder-alt-tag"
-              width="2000"
-              height="3000"
-              className="w-90 mb-0 mx-1 rounded-2xl hover:border-transparent hover:border"
+              width="1000"
+              height="1000"
+              className="h-full object-cover my-0 mx-1 rounded-2xl hover:border-transparent hover:border"
               loading="eager"
             />
           </li>
