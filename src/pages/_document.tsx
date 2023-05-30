25d523212c1739eb7874f850db046a4be2a96afb
Pat Wilson <pat@wilsons.org.nz>
2023-05-30 11:32:15 +1000

    all assets on main page updated, styled

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-      <body>
+      <body className="overflow-x-hidden">
         <Main />
         <NextScript />
       </body>
