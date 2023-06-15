47034289c51e835a7a554b08f6090ceaf96c7cb3
Pat Wilson <pat@wilsons.org.nz>
2023-06-15 20:50:28 +1000

    updating import/exports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Contact } from './landing-page/contact'
-import { Festival } from './landing-page/festivals'
-import { Gigs } from './landing-page/gigs'
-import { Main } from './landing-page/main'
-import { Nav } from './landing-page/nav'
-import { Travel } from './landing-page/travel'
+import Contact from './landing-page/contact'
+import Festival from './landing-page/festivals'
+import Gigs from './landing-page/gigs'
+import Main from './landing-page/main'
+import Nav from './landing-page/nav'
+import Travel from './landing-page/travel'
 import Layout from './travels/layout'
 
 export default function Home() {
--- src/pages/landing-page/travel.tsx
+++ src/pages/landing-page/travel.tsx
@@ -15,7 +15,7 @@ export default function Travel() {
         id="travel"
         className="widescreen:section-min-height tallscreen:section-min-height my-12 scroll-mt-20 p-6"
       >
-        <h2 className="mb-6 text-center font-caveat text-4xl text-6xl font-bold text-stone-800 dark:text-stone-100 sm:pl-60 sm:text-left sm:text-5xl">
+        <h2 className="mb-6 text-center font-caveat text-6xl font-bold text-stone-800 dark:text-stone-100 sm:pl-60 sm:text-left sm:text-5xl">
           Travel
         </h2>
         <ul className="mx-auto my-12 flex list-none flex-col justify-around gap-5 sm:flex-row">
@@ -49,7 +49,7 @@ export default function Travel() {
               loading="eager"
             />
           </li>
-          <li className="flex flex-col items-center rounded-3xl border border-2 bg-white px-2 py-2 shadow-xl dark:border-rose-950 dark:bg-stone-900 sm:w-5/6 md:w-1/5">
+          <li className="flex flex-col items-center rounded-3xl border bg-white px-2 py-2 shadow-xl dark:border-rose-950 dark:bg-stone-900 sm:w-5/6 md:w-1/5">
             <Image
               src="/images/travel/portrait-seville-6.jpeg"
               alt="placeholder-alt-tag"
