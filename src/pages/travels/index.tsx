a9a209470ef92727a7da4dcad1d8e79f0d313e0d
Pat Wilson <pat@wilsons.org.nz>
2023-06-15 20:34:48 +1000

    x

--- src/pages/travels/index.tsx
+++ src/pages/travels/index.tsx
@@ -1,13 +1,20 @@
-'use client'
-
 import 'leaflet/dist/leaflet.css'
 import MapNoSSR from '../../NoSSR/MapNoSSR'
 import Layout from './layout'
+import { useEffect } from 'react'
 
 export default function Home() {
-  return (
-    <Layout>
-      <MapNoSSR />
-    </Layout>
-  )
+  let toBeRendered = false
+
+  useEffect(() => {
+    toBeRendered = true
+  }, [])
+
+  if (toBeRendered) {
+    return (
+      <Layout>
+        <MapNoSSR />
+      </Layout>
+    )
+  } else return <></>
 }
