ae5297cd25e95df2cf287d3d2effa1c0b518b39a
Pat Wilson <pat@wilsons.org.nz>
2023-06-19 15:52:57 +1000

    testing map component

--- src/pages/travels/index.tsx
+++ src/pages/travels/index.tsx
@@ -1,8 +1,13 @@
 import 'leaflet/dist/leaflet.css'
 import MapNoSSR from '../../NoSSR/MapNoSSR'
 import Layout from './layout'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    console.log('map loading!')
+  })
+
   return (
     <Layout>
       <MapNoSSR />
