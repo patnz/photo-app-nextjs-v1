38809ea813d502fbfea89a1b9b2a1cb8bcb90dc0
Pat Wilson <pat@wilsons.org.nz>
2023-06-22 13:47:42 +1000

    ready to deploy

--- src/pages/travels/layout.tsx
+++ src/pages/travels/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: any) {
   return (
     <>
       <Nav />
-      <main className="no-scroll max-h-md fixed mx-auto my-auto flex h-screen w-screen justify-center overflow-hidden bg-stone-100 pt-10 dark:bg-stone-700">
+      <main className="no-scroll max-h-md fixed mx-auto my-auto flex h-screen w-screen justify-center overflow-hidden bg-stone-100 pt-20 dark:bg-stone-700">
         <motion.section
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
